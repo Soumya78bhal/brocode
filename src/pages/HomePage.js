d9ac72ddd62aac627d6afa9813de7c1ac2deb073
Soumya78bhal <soumya82bhal@gmail.com>
2023-05-08 22:32:38 +0530

    commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {v4 as uuidv4} from'uuid'
 
 const HomePage = () => {
+    const [roomId,SetRoomId]=useState('');
+    const [Username,SetUsername]=useState('');
+    const CreateNewRoom =(e)=>{
+        e.preventDefault();
+        
+        var id=uuidv4();
+        SetRoomId(id);
+    }
   return (
     <div className='HomePageWrapper'>
         <div className='FormWrapper'>
             <img src="/coding.png" className='logo' alt="logo" />
             <h4 className='MainLabel'>Enter Room Id and Username</h4>
             <div className='inputgroup'>
-                <input type="text" className='RoomId' placeholder='RoomId'/><br />
-                <input type="text" className='Username' placeholder='Username'/><br />
-                <button className='JoinButton'>Join</button><br />
+                <input 
+                    type="text" 
+                    className='RoomId' 
+                    placeholder='RoomId'  
+                    onChange={(e)=>SetRoomId(e.target.value)}
+                    value={roomId}
+                />
+                <input
+                    type="text" 
+                    className='Username' 
+                    placeholder='Username' 
+                    onChange={(e)=>SetUsername(e.target.value)}
+                    value={Username}
+                />
+                <button className='JoinButton'>Join</button>
                 <span className='newroom'>
                     To create new room &nbsp;
-                    <a href="">Click</a>
+                    <a onClick={CreateNewRoom} href="">Click</a>
                 </span>
             </div>
             
