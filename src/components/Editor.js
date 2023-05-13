a035cc817775d7fc7dc1413501d83457683b123e
Soumya78bhal <soumya82bhal@gmail.com>
2023-05-13 21:34:43 +0530

    commit

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,33 +1,42 @@
 import React, { useEffect, useRef } from 'react';
 import Codemirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/dracula.css';
+import 'codemirror/theme/midnight.css';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
 
 
 const Editor = () => {
-   
+    const useref=useRef(false)
     useEffect(() => {
+        
         async function init() {
            Codemirror.fromTextArea(
                 document.getElementById('realtimeEditor'),
                 {
                     mode: { name: 'javascript', json: true },
-                    theme: 'dracula',
+                    theme: 'midnight',
                     autoCloseTags: true,
                     autoCloseBrackets: true,
                     lineNumbers: true,
                 }
-            );
-
-           
+            );   
         }
-        init();
+        console.log("hello")
+        if(useref.current===false){
+          init();
+        }
+        return()=>{
+          useref.current=true;
+        }
+        
+      
     }, []);
 
-    return <textarea id="realtimeEditor"></textarea>;
+    return(
+       <textarea id="realtimeEditor"></textarea>
+    )
 };
 
 export default Editor;
